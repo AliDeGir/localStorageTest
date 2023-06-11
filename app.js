52e4d841458af75f003d37155b9e1798ffd8872c
AliDeGir <ali_deg@hotmail.com>
2023-06-11 14:04:46 +0200

    new login

--- app.js
+++ app.js
@@ -1,78 +1,48 @@
-function loggingIn() {
-    fulName = document.querySelector(".name").value;
-    fulPassword = document.getElementById("passInput").value;
+const employeeNr = [
+    701453731, 701507163, 701404090, 701402078, 701670115, 
+    701652729, 701430686, 701479498, 701630854, 701616623, 
+    701670346, 701406191, 701420502, 701545902, 701572269, 
+    701652705, 701660978, 701387056, 701493331, 701560865, 
+    701669374, 701519533, 701506379, 701535342, 701446851, 
+    701404477, 701556360, 701519679, 701519535, 701514112, 
+    701443922, 701556355, 701649475, 701537676, 701656135, 
+    701479876, 701518246, 701401775, 701540993, 701622446, 
+    701661774, 701534887, 701620607, 701424067, 701402434, 
+    701466553, 701401976, 701458552, 701662760, 701554777, 
+    701532255, 701473258, 701450718, 701664788, 701519955, 
+    701667069, 701550587, 701470181, 701631204
+];
 
-    if (fulName.length < 3) {
-        fulName = null
-    }
+function logIn() {
+    testPass = document.getElementById('passInput').value;
+    testName = document.getElementById("nameInput").value;
+    testPassNr = Number(testPass);
 
-    if (fulName == "" || fulName == null) {
-        alert("Either empty or null")
+    if (testName.length < 3) {
+        alert("Use 3 or more characters!");
     } else {
-        alert("Welcome " + fulName);
-        numberOfPassword = fulPassword.length;
-
-        if (numberOfPassword != 6) {
-            alert("not correct amount of passcode digits!")
+        if (testPass.length != 9) {
+            alert("This code requires 9 digits");
         } else {
-            storedName = localStorage.getItem('name');
-            storedPass = localStorage.getItem('pass');
-
-            if (fulPassword === storedPass) {
+            if (employeeNr.includes(testPassNr)) {
+                alert("Welcome " + testName + " :)");
                 document.getElementById('visibleDiv').style.background="yellow";
                 document.getElementById('hiddenDiv').style.display="flex";
                 document.getElementById('loginDiv').style.display="none";
+
+                if (localStorage.getItem('pass') != null) {
+                    localStorage.setItem('name', testName);
+                    localStorage.setItem('pass', testPassNr);
+                }
+                
             } else {
-                alert("No match found!");
+                alert("No match found");
+                localStorage.clear();
             }
         }
     }
 
     document.getElementById('nameInput').value="";
     document.getElementById('passInput').value="";
-}
 
-function newMember() {
-    fulName = document.querySelector(".name").value;
-    fulPassword = document.getElementById("passInput").value;
-
-    numberOfPassword = fulPassword.length;
-
-    if (fulName.length < 3) {
-        fulName = null;
-    }
-
-    if (fulName != "" || fulName != null) {
-        if (numberOfPassword == 6) {
-            alert(fulName + ", your credentials has been stored!")
-            localStorage.setItem('name', fulName);
-            localStorage.setItem('pass', fulPassword);
-        } else {
-            alert("Enter right amount of digits!")
-        }
-    } else {
-        alert("Nickname field empty of not enough characters!")
-    }
-
-    document.getElementById('nameInput').value="";
-    document.getElementById('passInput').value="";
-}
-
-function clearCache() {
-    localStorage.clear();
-    alert("Local storage cleared!")
-}
-
-function pageOnload() {
-    storedName = localStorage.getItem('name');
-    storedPass = localStorage.getItem('pass');
-
-    fulPassword = document.getElementById("passInput").value;
-
-    if (storedPass != null) {
-        document.getElementById('registerButton').style.display="none"
-        alert("Registered user! " + storedName + " " + storedPass)
-    } else {
-        alert("Not registered user!")
-    }
 }
